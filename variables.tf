e68f23de5051736d9ba00e86eaea818519ab100a
levente-simon <ls@leventesimon.com>
2022-02-20 19:11:33 +0100

    sensitive vars

--- variables.tf
+++ variables.tf
@@ -1,6 +1,4 @@
 variable "pm_api_host"         { type    = string }
-variable "pm_api_token_id"     { type    = string }
-variable "pm_api_token_secret" { type    = string }
 variable "pm_api_port"         { type    = string }
 variable "dns_server"          { type    = string }
 variable "dns_port"            { type    = string }
@@ -8,3 +6,11 @@ variable "searchdomain"        { type    = string }
 variable "cluster_hosts"       { type    = map(any) }
 variable "os_user"             { type    = string }
 
+variable "pm_api_token_id" {
+  type      = string
+  sensitive = true
+}
+variable "pm_api_token_secret" {
+  type      = string
+  sensitive = true
+}
